201d7afd4fcd2174f590fb1219e6f416e808ac18
CSLad <88428000+CSLad@users.noreply.github.com>
2021-08-04 14:44:51 +0600

    Delete src/main/java directory

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import java.io.FileNotFoundException;
-
-public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        FuncCall call = new FuncCall();
-        call.allCall();
-    }
-}
